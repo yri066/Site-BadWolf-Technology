82fcbc89d4045e50b731bf1b61121dd2aeabaa3c
yri066 <yri066.ru@gmail.com>
2024-06-06 21:28:17 +0300

    Добавлена конфигурация и миграция для контента и продуктов.

--- BadWolfTechnology/Data/ApplicationDbContext.cs
+++ BadWolfTechnology/Data/ApplicationDbContext.cs
@@ -17,9 +17,52 @@ namespace BadWolfTechnology.Data
         public DbSet<News> News { get; set; }
         public DbSet<Comment> Comments { get; set; }
 
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Product> Products { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<Post>(x =>
+            {
+                x.Property(b => b.Title)
+                    .HasMaxLength(100);
+
+                x.Property(b => b.CodePage)
+                    .HasMaxLength(50);
+
+                x.HasIndex(b => b.CodePage);
+
+                x.Property(b => b.Contents)
+                    .HasConversion(
+                        v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+                        v => JsonSerializer.Deserialize<List<Content>>(v, (JsonSerializerOptions)null),
+                        new ValueComparer<IList<Content>>(
+                            (c1, c2) => c1.Equals(c2),
+                            c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                            c => (IList<Content>)c.ToList()));
+            });
+
+            builder.Entity<Product>(x =>
+            {
+                x.Property(b => b.Title)
+                    .HasMaxLength(100);
+
+                x.Property(b => b.CodePage)
+                    .HasMaxLength(50);
+
+                x.HasIndex(b => b.CodePage);
+
+                x.Property(b => b.Contents)
+                    .HasConversion(
+                        v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+                        v => JsonSerializer.Deserialize<List<Content>>(v, (JsonSerializerOptions)null),
+                        new ValueComparer<IList<Content>>(
+                            (c1, c2) => c1.Equals(c2),
+                            c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                            c => (IList<Content>)c.ToList()));
+            });
 
             builder.Entity<Comment>(x =>
             {
---
+++ BadWolfTechnology/Data/Migrations/20240511194559_Posts.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BadWolfTechnology.Data.Migrations
+{
+    public partial class Posts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsView = table.Column<bool>(type: "bit", nullable: false),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CodePage = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Contents = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
