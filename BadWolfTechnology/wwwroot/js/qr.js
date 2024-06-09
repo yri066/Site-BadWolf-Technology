e263ac05473a45f64a68983cda79efe967e8fcbb
yri066 <yri066.ru@gmail.com>
2024-06-09 19:22:52 +0300

    Добавлена генерация QR кода для 2FA.

---
+++ BadWolfTechnology/wwwroot/js/qr.js
@@ -0,0 +1,12 @@
+﻿window.addEventListener("load", () => {
+    const uri = document.getElementById("qrCodeData").getAttribute('data-url');
+    new QRCode(document.getElementById("qrCode"),
+        {
+            text: uri,
+            width: 180,
+            height: 180,
+            colorDark: "#000000",
+            colorLight: "#ffffff",
+            correctLevel: QRCode.CorrectLevel.L
+        });
+});
