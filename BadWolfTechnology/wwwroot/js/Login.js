199a0b405a6b26190cf6df5e340fd0308cd18b3d
yri066 <yri066.ru@gmail.com>
2024-11-11 21:28:26 +0300

    Обновлен скрипт авторизации.

--- BadWolfTechnology/wwwroot/js/Login.js
+++ BadWolfTechnology/wwwroot/js/Login.js
@@ -1,38 +1,42 @@
-﻿function loginAuth() {
-
+﻿async function loginAuth() {
+    const loginError = "Ошибка входа.";
 
     var formData = new FormData(document.getElementById("account-login"));
     formData.append("returnPathName", window.location.pathname);
     formData.set("RememberMe", document.getElementById("RememberMe").checked);
 
-        fetch(`${window.location.origin}/Account/Login`, {
-            method: "POST",
-            body: formData,
-        })
-            .then(response => {
-
-            if (response.redirected) {
-                document.getElementById("auth-error").style.display = "none";
-                window.location.replace(response.url);
-                return "";
-            }
-            else if (response.ok) {
-                return response.text();
-            }
-        })
-        .then(json => {
-            if (JSON.parse(json).url) {
-                document.getElementById("auth-error").style.display = "none";
-                window.location.replace(JSON.parse(json).url);
-            }
-
-            if (json != undefined || json != "") {
-                console.log();
-                showToast(JSON.parse(json).error);
-                document.getElementById("auth-error").innerText = JSON.parse(json).error;
-                document.getElementById("auth-error").style.display = "block";
-            }
-        })
-            .catch(error => {
+    try {
+        // Send a POST request to /Account/Login
+        const response = await fetch('/Account/Login', {
+            method: 'POST',
+            body: formData
         });
+
+        const contentType = response.headers.get('Content-Type') || '';
+        if (contentType.includes('application/json')) {
+
+            const result = await response.json();
+
+            if (result.error) {
+                // Если авторизация не удалась, вывести сообщение об ошибке.
+                showError(loginError);
+            }
+        } else if (response.ok) {
+            // При успешной авторизации, производит перенаправление пользователя.
+            document.getElementById("auth-error").style.display = "none";
+            window.location.href = response.url;
+        } else {
+            // Если авторизация не удалась, вывести сообщение об ошибке.
+            showError(loginError);
+        }
+    } catch (error) {
+        showError(loginError);
+    }
+}
+
+function showError(errorText) {
+    // Если авторизация не удалась, вывести сообщение об ошибке.
+    showToast(errorText);
+    document.getElementById("auth-error").innerText = errorText;
+    document.getElementById("auth-error").style.display = "block";
 }
